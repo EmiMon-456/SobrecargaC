1b2935e45723a8002694a729d7ae75c3db51cf50
Emiliano Montalvo <A00574526@tec.mx>
2025-05-15 12:20:33 -0600

    Add main.cpp with implementation of Complejo class and operator overloading

---
+++ sobrecarga-Emiliano-Montalvo/main.cpp
@@ -0,0 +1,26 @@
+//
+//main.cpp
+//Ejercicio 1
+// Jesús Emiliano Hernández Montalvo
+// Created by Emiliano Hernández Montalvo on 2025/05/15
+//
+
+#include <iostream>
+#include <string>  
+using namespace std;
+
+class Complejo {
+public:
+    double re, in;
+    Complejo(double x, double y) : re(x), in(y) {}
+    Complejo operator+(const Complejo& otro) {
+        return Complejo(re + otro.re, in + otro.in);
+    }
+};
+
+int main() {
+    Complejo c1(1.0, 2.0), c2(3.0, -1.0);
+    Complejo resultado = c1 + c2;
+    cout << "Resultado: (" << resultado.re << ", " << resultado.in << ")" << endl;
+    return 0;
+}
