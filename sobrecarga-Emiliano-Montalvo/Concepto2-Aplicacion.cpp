1c8686d0f4ae3c036d4873255c8c670d7ab7efe3
Emiliano Montalvo <A00574526@tec.mx>
2025-05-16 15:17:06 -0600

    Remove obsolete files and add new implementations for the Punto class with operator overloading

---
+++ sobrecarga-Emiliano-Montalvo/Concepto2-Aplicacion.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Punto {
+public:
+    int x, y;
+    Punto(int _x, int _y) : x(_x), y(_y) {}
+    bool operator==(const Punto& p) {
+        return x == p.x && y == p.y;
+    }
+};
+
+int main() {
+    Punto a(1,2), b(1,2), c(2,2);
+    cout << std::boolalpha;
+    cout << (a == b) << endl;
+    cout << (a == c) << endl; 
+}
