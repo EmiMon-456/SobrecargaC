31dae1c76374cfbf3e19524beb297a5d3daebb8d
Emiliano Montalvo <A00574526@tec.mx>
2025-05-19 14:51:21 -0600

    Add header comments to Ejercicio2H.cpp through Ejercicio12H.cpp and Ejercicio7H.cpp

---
+++ Soluciones-Herencia-Emiliano-Montalvo-A00574526/Ejercicio2H.cpp
@@ -0,0 +1,5 @@
+// 
+// Ejercicio1H.cpp
+// Created by Jesús Emiliano Hernández Montalvo on 19/5/25.
+// A00574526
+// 
---
+++ Soluciones-Herencia-Emiliano-Montalvo-A00574526/Ejercicio7H.cpp
@@ -0,0 +1,18 @@
+// 
+// Ejercicio1H.cpp
+// Created by Jesús Emiliano Hernández Montalvo on 19/5/25.
+// A00574526
+// 
+#include <iostream>
+using namespace std;
+
+class instrumento {
+    public: 
+    virtual void tocar() {
+
+    }
+};
+
+class guitarra: public instrumento {
+    public 
+}
