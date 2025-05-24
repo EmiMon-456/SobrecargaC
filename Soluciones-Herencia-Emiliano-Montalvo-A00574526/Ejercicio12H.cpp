9a35bb0ea06237cf3211c371bf34126cfadb14c9
Emiliano Montalvo <A00574526@tec.mx>
2025-05-24 00:00:38 -0600

    Implement ControlRemoto class with constructors and add output to Salidas.txt

--- Soluciones-Herencia-Emiliano-Montalvo-A00574526/Ejercicio12H.cpp
+++ Soluciones-Herencia-Emiliano-Montalvo-A00574526/Ejercicio12H.cpp
@@ -3,3 +3,53 @@
 // Created by Jesús Emiliano Hernández Montalvo on 19/5/25.
 // A00574526
 // 
+#include <iostream>
+using namespace std;
+
+// Crear una clase base llamada Dispositivo
+class Dispositivo{
+    // Atributos públicos de la clase
+    public:
+    // Definir el tipo de variable
+        string marca;
+    //Constructor de la clase Dispositivo
+    // Lleva un apuntador dentro para evitar generar errores
+        Dispositivo(const string& _marca = ""):marca(_marca){
+            marca = _marca;
+    }
+};
+
+// Crear una clase derivada llamada TV que herede de Dispositivo
+class TV: virtual public Dispositivo{
+    //atributos públicos de la clase
+    public:
+    //establecer un constructor
+    TV(){};    
+};
+
+// Crear una clase derivada llamada DVD que herede de Dispositivo
+class DVD: virtual public Dispositivo{
+    //atributos públicos de la clase
+    public:
+    //establecer un constructor
+    DVD(){};
+};
+
+// Crear una clase derivada que herede de las derivadas TV y DVD
+// Esta clase se llamará ControRemoto
+
+class ControlRemoto: public TV, public DVD{
+    //atributos públicos de la clase
+    public:
+    //establecer un constructor
+    ControlRemoto(){};
+};
+
+int main(){
+    // crear un objeto de la clase ControlRemoto
+    ControlRemoto control;
+    // establecer la marca del control remoto
+    control.marca = "Samsung";
+    // mostrar la marca del control remoto 
+    cout << "Marca del control remoto: " << control.marca << std::endl;
+};
